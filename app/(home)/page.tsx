b90d7e825bb3c60ad459a505c2afe90c6436baff
Gabriela Florêncio <perflogabi@gmail.com>
2024-05-30 19:41:23 -0300

    feat: add initial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
 import { format } from "date-fns";
+import Search from "./_components/search";
+
 
 export default function Home() {
   return (
@@ -15,6 +17,10 @@ export default function Home() {
             locale: ptBR,
           })}
         </p>
+      </div>
+      
+      <div className="px-5 mt-6">
+        <Search/>
       </div>
     </div>
   );
